cafac1672e2cd3feff4e98ab293ea75b178b5a65
kamilsamaj <kamilsamaj@gmail.com>
2021-02-23 18:28:17 +0100

    xhr to icndb.com works

---
+++ app.js
@@ -0,0 +1,28 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+    // number of jokes to fetch from the API
+    const jokes = document.querySelector('input[type="number"]').value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.icndb.com/jokes/random/${jokes}`, true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+
+            let output = '';
+            if (response.type === 'success') {
+                response.value.forEach(joke => {
+                    output += `<li>${joke.joke}</li>`;
+                });
+            } else {
+                output += '<li>Something went wrong when fetching jokes from https://icndb.com</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+        }
+    };
+    xhr.send();
+    e.preventDefault();
+}
